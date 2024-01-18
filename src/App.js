451253d87772a0607b5cfaaa87bf28013be57321
Salman Maarouf <salman.k.maarouf@gmail.com>
2024-01-18 14:37:41 +1100

    second progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Who said it?</h1>
         <button>New game</button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
 
 export default App;
+
